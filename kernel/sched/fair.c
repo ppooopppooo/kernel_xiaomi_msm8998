ae53cb42b92fa99ca786243b6e6ae884ed528134
ppooopppooo <ppooopppooo@qq.com>
2019-05-18 15:46:07 +0800

    Revert "sched/fair: replace some bits with CAF's modified commit"
    This reverts commit 2ae58d8eb1a9212b8b30c95313d5922f39d51f37.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7740,8 +7740,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 				 * IOW, prefer a deep IDLE LITTLE CPU vs a
 				 * shallow idle big CPU.
 				 */
-				if (capacity_orig >= target_capacity &&
-				    sysctl_sched_cstate_aware &&
+				if (sysctl_sched_cstate_aware &&
 				    best_idle_cstate <= idle_idx)
 					continue;
 
@@ -7773,8 +7772,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			 */
 
 			/* Favor CPUs with maximum spare capacity */
-			if (capacity_orig >= target_capacity &&
-			    spare_cap < target_max_spare_cap)
+			if ((capacity_orig - new_util) < target_max_spare_cap)
 				continue;
 
 			target_max_spare_cap = capacity_orig - new_util;
